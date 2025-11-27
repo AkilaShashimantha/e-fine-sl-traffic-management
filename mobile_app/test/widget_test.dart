349d53ba9b983aac0d18aeb460590f86d5ab4810
Akila Shashimantha <akilashashimantha84@gmail.com>
2025-11-27 18:17:32 +0530

    update text widget

--- mobile_app/test/widget_test.dart
+++ mobile_app/test/widget_test.dart
@@ -1,13 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mobile_app/main.dart'; // මෙතන mobile_app වෙනුවට ඔබේ project එකේ නම (pubspec.yaml එකේ ඇති නම) හරියට තියෙන්න ඕනේ.
+import 'package:mobile_app/main.dart'; 
 
 void main() {
   testWidgets('App launch smoke test', (WidgetTester tester) async {
-    // 1. අපේ ඇප් එක ලෝඩ් කරන්න
+  
     await tester.pumpWidget(const EFineApp());
 
-    // 2. Splash Screen එකේ "E-Fine SL" කියන නම තියෙනවද බලන්න
-    // (අපි Splash Screen එකේ ඒ නම දැම්මා මතක ඇති)
+   
     expect(find.text('E-Fine SL'), findsOneWidget);
   });
 }
